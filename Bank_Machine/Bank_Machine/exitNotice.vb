1b15d90f2f0f741f413bc36a9797bbea7e2f39ce
Sammy Jackson <sam.jackson@live.ca>
2017-11-23 05:43:40 -0500

    logout page exit update
    logout page actually closes with timer now YAY!

--- Bank_Machine/Bank_Machine/exitNotice.vb
+++ Bank_Machine/Bank_Machine/exitNotice.vb
@@ -1,12 +1,12 @@
 ﻿Public Class exitNotice
     Dim second As Integer
 
-    Private Sub test_Load(ByVal sender As System.Object, ByVal e As System.EventArgs)
+    Private Sub test_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Timer1.Interval = 1000
         Timer1.Start() 'Timer starts functioning
     End Sub
 
-    Private Sub Timer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
         second = second + 1
         If second >= 5 Then
             Timer1.Stop() 'Timer stops functioning
@@ -15,7 +15,7 @@
 
     End Sub
 
-    Private Sub Timer_Tick_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+    Private Sub Timer1_Tick_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
 
     End Sub
 End Class
